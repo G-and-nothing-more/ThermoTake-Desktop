42b5237badae340504580d7b60bda0ff5dcc249a
RupB <59860448+RupB@users.noreply.github.com>
2022-02-15 00:51:06 -0600

    added msgAlert

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Top-level package for the ThermoApp."""
 
+import smtplib
+from email.message import EmailMessage
 import kivy
 import re
 # from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
@@ -44,15 +46,46 @@ class ThermoApp(App):
         """Buttonpress method for Send Message."""
         print("bort")
         area, code1, code2 = self.getTLFN()
+        
+        #self.getTLFN is a tuple of string??? 
+        #if so then will convert that to string later #TODO for Rupanti
+
         print(area)
         print(code1)
         print(code2)
+
+        #### how to check the temperature??????? 
+        # if temp>300F then
+        self.msgAlert("Alert","Tempterature is above 300F", "--------@txt.att.net")
+
+ 
 
     def getTLFN(self):
         """Return a tuple of the 3 parts of a phone number."""
         m = re.search(r'\(?(\d{3})[) \- ]?(\d{3})[- ]?(\d{4})',
                       self.root.phoneNum)
         return (m.group(1), m.group(2), m.group(3))
+
+
+   ##message alert function
+    def msgAlert(subject, body,to):
+        msg=EmailMessage()
+        msg.set_content(body)
+        msg['subject']=subject
+        msg['to']=to
+        user="-----------@gmail.com"
+        msg['from']=user
+        password="--------"
+
+        ##setting server
+        server=smtplib.SMTP("smtp.gmail.com",587)
+        server.starttls()
+        server.login(user,password)
+        server.send_message(msg)
+        server.quit()
+        
+
+
 
 
 
